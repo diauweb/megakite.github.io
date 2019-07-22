9c440803aa72f35a283f1a303ca4f414fa6a15b1
Xiaodai Dai <diauweb@live.com>
2019-07-22 17:01:24 +0800

    Add comment format

--- themes/kaito/scripts/special-formats.js
+++ themes/kaito/scripts/special-formats.js
@@ -1,3 +1,7 @@
 hexo.extend.tag.register('fsmall', function(_,content) {
     return `<p class="sml">${content}</p>`
 }, {ends: true})
+
+hexo.extend.tag.register('fcomment', function(_,content) {
+    return `<p class="cmt">${content}</p>`
+}, {ends: true})
